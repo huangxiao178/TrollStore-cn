702b1746886bcb8ab971a6a3779d6893263bf10c
huangxiao178 <huangxiao178@users.noreply.github.com>
2026-07-11 12:10:14 +0800

    fix headers + devmode stub

--- RootHelper/devmode.m
+++ RootHelper/devmode.m
@@ -1,3 +1,3 @@
 #import "devmode.h"
 BOOL checkDeveloperMode(void) { return YES; }
-BOOL armDeveloperMode(BOOL* alreadyEnabled) { if(alreadyEnabled) *alreadyEnabled=YES; return YES; }
+BOOL armDeveloperMode(BOOL* e) { if(e) *e=YES; return YES; }

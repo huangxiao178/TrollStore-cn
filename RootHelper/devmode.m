7bfd7d2d19d1ff1cccc1035a92741dd2edf65bff
huangxiao178 <huangxiao178@users.noreply.github.com>
2026-07-11 12:00:46 +0800

    ALL FIXES: devmode stub + fat.h fix + macos-14

--- RootHelper/devmode.m
+++ RootHelper/devmode.m
@@ -1,4 +1,4 @@
-﻿// Stub: SDK 17.5 unavailable fix
+﻿// Stub for SDK 17.5
 #import "devmode.h"
 BOOL checkDeveloperMode(void) { return YES; }
 BOOL armDeveloperMode(BOOL* alreadyEnabled) { if(alreadyEnabled) *alreadyEnabled=YES; return YES; }

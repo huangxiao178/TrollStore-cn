c2e78509f6d5d35a2b649b8d69aad2ee7da7f2ef
Jumo Build <noreply@localhost>
2026-08-30 00:05:01 +0800

    Cast scene root controller to license-aware type

--- TrollStore/TSSceneDelegate.m
+++ TrollStore/TSSceneDelegate.m
@@ -196,7 +196,7 @@
 - (void)sceneDidBecomeActive:(UIScene *)scene {
 	// Called when the scene has moved from an inactive state to an active state.
 	// Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-	[_rootViewController refreshJumoLicense];
+	[(TSRootViewController *)_rootViewController refreshJumoLicense];
 }
 
 
